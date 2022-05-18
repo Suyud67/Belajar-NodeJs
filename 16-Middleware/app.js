50f63b6bfeec00a6ee7ddff8fe215b1ba3d36543
suyud67 <suyudsetiawan@gmail.com>
2022-05-18 10:34:38 +0800

    first commit

---
+++ 16-Middleware/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// menggunakan ejs
+app.set('view engine', 'ejs');
+
+// Built-in middleware
+app.use(express.static('public'));
+
+// application-level middleware
+app.use((req, res, next) => {
+  console.log('Hello ini middleware');
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.render('index', { header: 'Halaman Utama' });
+});
+
+app.get('/detail', (req, res) => {
+  res.render('detail', { detailHeader: 'Halaman Detail' });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { aboutHeader: 'Halaman About' });
+});
+
+app.get('/about/product/:nama', (req, res) => {
+  res.send(`product dengan nama: ${req.params.nama} dan nama produknya adalah: ${req.query.jml}`);
+});
+
+app.use('/', (req, res) => {
+  res.status(404);
+  res.send('<h1>Page Not Found!</h1>');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
